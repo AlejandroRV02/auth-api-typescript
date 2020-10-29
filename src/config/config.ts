b0aa872b31680f0ebe7a93a86e0d55d19346c6ef
Alejandro <alejandrorv944@gmail.com>
2020-10-29 14:41:05 -0600

    Change

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 
 
 export default {
-    URI: process.env.URI || 'mongodb+srv://alex13:alexrv13@cluster0.48b4z.mongodb.net/node-api-typescript?retryWrites=true&w=majority'
+    URI: process.env.URI || 'localhost/db'
     ,
     SECRET: process.env.SECRET || 'secretxd'
 }
